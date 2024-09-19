e3070324ae1bd947dfb639aadf462c64f291786a
Asaad Jaber <asaadmjaber@gmail.com>
2024-09-19 16:37:21 +0300

    deleted commented lines

--- JSUHealth/JSUHealth/Modules/Clinics/ClinicCardViewModel.swift
+++ JSUHealth/JSUHealth/Modules/Clinics/ClinicCardViewModel.swift
@@ -15,15 +15,6 @@ class ClinicCardViewModel {
         Item.clinicCard(ClinicCard(name: "Acıbadem Bursa Hospital", imageName: "hospital", location: "Bursa"))
     ]
     
-//    ClinicCard(name: "Acibadem Istanbul", imageName: "medical clinic", location: "Istanbul"),
-//    ClinicCard(name: "Turkiye Hospital", imageName: "dentistry", location: "Istanbul"),
-//    ClinicCard(name: "Civas Clinic", imageName: "surgery", location: "Ankara"),
-//    ClinicCard(name: "Adaport Dental Clinic", imageName: "dental clinic", location: "Izmir"),
-//    ClinicCard(name: "Memorial Hospital", imageName: "hospital", location: "Ankara"),
-//    ClinicCard(name: "Lokman Hekim University Ankara Hospital", imageName: "hospital", location: "Ankara"),
-//    ClinicCard(name: "Antalya Medical Park Clinic", imageName: "medical center", location: "Antalya"),
-//    ClinicCard(name: "Izmir Can Hospital", imageName: "hospital", location: "Izmir"),
-    
     let photoProvider = PhotoProvider()
 
     func makeImageQuery(query: String) async -> (Data?, URLResponse?) {
